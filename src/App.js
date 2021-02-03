1b728a8c7c96e2911995da46f765f67baea226ff
Ruslan Kumitskiy <ruslankum2@gmail.com>
2021-02-03 16:46:42 +0300

    lvlup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import './App.css';
-import Footer from './Footer';
-import Header from './Header';
-import Technologies from './Technologies';
 
 const App = () => {
   return (
-    <div>
-      <Header></Header>
-      <Technologies></Technologies>
-      <Footer></Footer>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img className='header-img' src='https://cdn.motor1.com/images/mgl/GwZbJ/s3/logo-story-volkswagen.jpg'></img>
+      </header>
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src='https://cdn24.img.ria.ru/images/151794/84/1517948413_0:105:2000:1230_600x0_80_0_0_e12706701a13b6260e2fa5f2e4626c94.jpg'></img>
+        </div>
+        <div>ava + description</div>
+        <div>
+          my posts
+          <div>
+            new posts
+          </div>
+          <div>
+            <div>post 1</div>
+            <div>post 2</div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
