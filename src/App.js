76e5ce8719a73e9508e865901135dee43640b7a5
Ruslan Kumitskiy <ruslankum2@gmail.com>
2021-02-03 15:22:01 +0300

    lesson +1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div>
-      <div>one div</div>
-      <div className="App">
+      <Header />
+      <Technologies />
+    </div>
+  )
+}
+
+const Technologies = () => {
+  return (
+      <div>
         <ul>
           <li>css</li>
           <li>html</li>
@@ -12,6 +19,15 @@ function App() {
           <li>react</li>
         </ul>
       </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <div>
+      <a href='#s'>Home</a>
+      <a href='#s'>News Feed</a>
+      <a href='#s'>Messages</a>
     </div>
   );
 }
