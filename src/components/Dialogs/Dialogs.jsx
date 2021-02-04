3dda5ff09e6ba3546117a2ad30086559d41be729
Ruslan Kumitskiy <ruslankum2@gmail.com>
2021-02-04 19:06:06 +0300

    ++

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,32 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div className={s.content}>
-            privet
+        <div className={s.dialogs}> {/*"dialogs"*/}
+            <div className={s.dialogs_items}> {/*"items"*/}
+                <div className={s.dialog + ' ' + s.active}>
+                    Dimych
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    Andrey
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    Sveta
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    Sasha
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    Viktor
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    Valera
+                </div>
+            </div>
+            <div className={s.messages}> {/*messages*/}
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Weeeey</div>
+            </div>
         </div>
     );
 }
