707bd8db56eac3d30acecfb988dffc99afdc7be2
Ruslan Kumitskiy <ruslankum2@gmail.com>
2021-02-05 16:40:35 +0300

    render={() => </>}...

--- src/App.js
+++ src/App.js
@@ -15,11 +15,11 @@ const App = () => {
         <Header></Header>
         <Navbar></Navbar>
         <div className='app-wrapper-content'>
-          <Route path='/profile' component={Profile}></Route>
-          <Route path='/dialogs' component={Dialogs}></Route>
-          <Route path='/news' component={News}></Route>
-          <Route path='/music' component={Music}></Route>
-          <Route path='/settings' component={Settings}></Route>
+          <Route path='/profile' render={() => <Profile/>}></Route>
+          <Route path='/dialogs' render={() => <Dialogs/>}></Route>
+          <Route path='/news' render={() => <News/>}></Route>
+          <Route path='/music' render={() => <Music/>}></Route>
+          <Route path='/settings' render={() =><Settings/>}></Route>
         </div>
       </div>
     </BrowserRouter>
