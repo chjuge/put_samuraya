ebee746d7f4bbada7c9a4428206e39afe9764929
Ruslan Kumitskiy <ruslankum2@gmail.com>
2021-02-06 19:30:19 +0300

    ++

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import './index.css';
-import state from './redux/state.js';
+import store from './redux/state.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals';
-import { addPost, updateNewText, subscribe } from './redux/state.js';
 
 let rerenderEntireTree = (state) => {
 
@@ -12,14 +11,14 @@ let rerenderEntireTree = (state) => {
         <React.StrictMode>
             <App
                 state={state}
-                addPost={addPost}
-                updateNewText={updateNewText} />
+                addPost={store.addPost.bind(store)}
+                updateNewText={store.updateNewText.bind(store)} />
         </React.StrictMode>,
         document.getElementById('root')
     );
 
     reportWebVitals();
 }
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,54 +1,54 @@
-let state = {
-    profilePage: {
-        posts: [
-            { id: 1, message: 'Hi! Hi, how are you?', likes: 15 },
-            { id: 2, message: 'It is my first post', likes: 40 }
-        ],
-        newPostText: 'it-kamasutra.huy'
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                { id: 1, message: 'Hi! Hi, how are you?', likes: 15 },
+                { id: 2, message: 'It is my first post', likes: 40 }
+            ],
+            newPostText: 'it-kamasutra.huy'
+        },
+        dialogsPage: {
+            dialogs: [
+                { id: 1, name: 'Dimych' },
+                { id: 2, name: 'Andrey' },
+                { id: 3, name: 'Sveta' },
+                { id: 4, name: 'Sasha' },
+                { id: 5, name: 'Viktor' },
+                { id: 6, name: 'Valera' }
+            ],
+            messages: [
+                { id: 1, message: 'Hi!' },
+                { id: 2, message: 'How are you?' },
+                { id: 3, message: 'Ready to fight????' },
+                { id: 4, message: 'Yo' },
+                { id: 5, message: 'Yo' },
+                { id: 6, message: 'Yo' }
+            ]
+        }
     },
-    dialogsPage: {
-        dialogs: [
-            { id: 1, name: 'Dimych' },
-            { id: 2, name: 'Andrey' },
-            { id: 3, name: 'Sveta' },
-            { id: 4, name: 'Sasha' },
-            { id: 5, name: 'Viktor' },
-            { id: 6, name: 'Valera' }
-        ],
-        messages: [
-            { id: 1, message: 'Hi!' },
-            { id: 2, message: 'How are you?' },
-            { id: 3, message: 'Ready to fight????' },
-            { id: 4, message: 'Yo' },
-            { id: 5, message: 'Yo' },
-            { id: 6, message: 'Yo' }
-        ]
+    getState() {
+        return this._state;
+    },
+    _callSubscriber() {
+        console.log('Stage changed');
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likes: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    },
+    updateNewText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
     }
-};
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
 }
-
-let rerenderEntireTree = () => {
-    console.log('Stage changed');
-}
-
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likes: 0
-    };
-
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-}
-
-export const updateNewText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export default state;
+export default store;
+window.store = store;
