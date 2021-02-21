c8e0f9ef54731600cfddbea7b035c42f0a277387
Ruslan Kumitskiy <ruslankum2@gmail.com>
2021-02-21 19:52:51 +0300

    setState, local state

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import preloader from "../../../assets/images/loading.svg";
+import ProfileStatus from './ProfileStatus';
 
 
 const ProfileInfo = (props) => {
@@ -13,9 +14,10 @@ const ProfileInfo = (props) => {
     }
     return (
         <div>
-            <div>
+            {/* <div>
                 <img src='https://cdn24.img.ria.ru/images/151794/84/1517948413_0:105:2000:1230_600x0_80_0_0_e12706701a13b6260e2fa5f2e4626c94.jpg'></img>
-            </div>
+            </div> */}
+            <ProfileStatus status='Hi niggas!'/>
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large} />
             </div>
